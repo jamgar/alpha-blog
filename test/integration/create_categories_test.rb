20bb3da76e5320248bd493f10ae66c2d1956002e
James <jegpub@gmail.com>
2016-03-15 10:48:48 +0000

    add admin requirement to create categories

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -22,8 +22,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     end
     assert_template 'categories/new'
     assert_select 'h2.panel-title'    
-    assert_select 'div.panel-body
-    '
+    assert_select 'div.panel-body'
   end
   
 end
